1d301a6cd45d92a12f063a7f8675f9f568c6af64
Angela <achimon0907@g.educaand.es>
2025-10-17 20:41:02 +0200

    Html hecho, falta css y js

---
+++ Optativa WebFullStack/FormularioValidacion/js/validacion.js
@@ -0,0 +1,19 @@
+
+
+// ocultar y mostrar elementos despues de validar lo anterior
+
+// validarción nombre
+
+// validarción apellidos
+
+// validación email
+
+// validación contraseña
+
+// validación edad
+
+// validación país
+
+// validación términos y condiciones    
+
+// cuando todo lo anterior sea validado, enviar formulario
