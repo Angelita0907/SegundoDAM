6fc2eb136d15bb76b44721ee7d95f2cfba187502
Angela <achimon0907@g.educaand.es>
2025-10-30 10:17:58 +0100

    python acabado

--- Sistema Gestion Empresarial/Pyhon/Tema1/Objetivo7/objetivo7_fase123_ejercicio2.py
+++ Sistema Gestion Empresarial/Pyhon/Tema1/Objetivo7/objetivo7_fase123_ejercicio2.py
@@ -25,6 +25,13 @@ class Video:
     def GetCategoria(self):
         return self.__categoria
     
+    def mirar_video(self):
+        print("¡Iniciando video!")
+        print("El video se llama: ", self.GetTitulo_V(), " con duracion de ", self.GetDuracion(), " y su categoria es ", self.GetCategoria())
+
+    def detener_video(self):
+        print("Deteniendo el video")
+
 class Audio:
     def __init__(self, titulo_A = "", nombre = ""):
         self.__titulo_A = titulo_A
@@ -36,5 +43,36 @@ class Audio:
     def GetTitulo_A(self):
         return self.__titulo_A
     
+    def SetNombre(self, nombre):
+        self.__nombre = nombre
     
+    def GetNombre(self):
+        return self.__nombre
     
+    def escuchar_audio(self):
+        print("Escuchando audioo!")
+        print("El audio se titula ",self.GetTitulo_A(), " producido por el artista ", self.GetNombre())
+    
+    def detener_audio(self):
+        print("Finalizando reproduccion de audio")
+
+# clase hija que hereda de las dos anteriores y llama a las funciones
+class Media(Video, Audio):
+    def __init__(self, titulo_V, categoria, duracion, nombre):
+        # debemos iniciar los constructores de los padre para que pueda coger los valores
+        Video.__init__(self, titulo_V, duracion, categoria)
+        Audio.__init__(self, titulo_V, nombre)
+
+    def mostrar_media(self):
+        print("--- REPRODUCCIÓN DE MEDIA ---")
+        self.mirar_video()
+        self.detener_video()
+        self.escuchar_audio()
+        self.detener_audio()
+
+# ahora mostraremos las funciones de la clase media que hereda de los anteriores
+medio1 = Media("Titulo 1", "infantil", 180, "Artista 1")
+medio1.escuchar_audio()
+medio1.mirar_video()
+medio1.detener_audio()
+medio1.detener_video()
