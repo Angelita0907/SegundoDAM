f93fba05df3e6b33697868f6013128f041fa15c8
Angela <achimon0907@g.educaand.es>
2025-11-06 13:15:15 +0100

    terminar ejercicio

--- Programacion Multimedia/AppEjemplo/app/src/main/java/com/example/appejemplo/Activity2.java
+++ Programacion Multimedia/AppEjemplo/app/src/main/java/com/example/appejemplo/Activity2.java
@@ -11,7 +11,7 @@ public class Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_actividad2);
+        setContentView(R.layout.pruebalinearlayout);
 
         Bundle b = getIntent().getExtras();
 
