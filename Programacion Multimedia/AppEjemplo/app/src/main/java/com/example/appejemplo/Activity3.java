7975a03d99210c9ba408f4093fd347f96f7f6d52
Angela <achimon0907@g.educaand.es>
2025-11-23 19:02:38 +0100

    boletin fonts y animaciones

--- Programacion Multimedia/AppEjemplo/app/src/main/java/com/example/appejemplo/Activity3.java
+++ Programacion Multimedia/AppEjemplo/app/src/main/java/com/example/appejemplo/Activity3.java
@@ -1,4 +1,23 @@
 package com.example.appejemplo;
 
-public class Activity3 {
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pruebalinearlayout);
+
+        TextView miTexto= (TextView) findViewById(R.id.texto1);
+        miTexto.setText("Nuevo texto para mostrar");
+
+
+    }
+
 }
