3377c9b127849e28b3d036237350afc3c9bdda4a
Angela <achimon0907@g.educaand.es>
2025-11-23 18:07:04 +0100

    ejercicio mongo

--- Acceso a Datos/Mongo/playground-1.mongodb.js
+++ Acceso a Datos/Mongo/playground-1.mongodb.js
@@ -16,4 +16,26 @@ use('peliculas');
 //db.estudiantes.find().sort({"notaMedia":-1, "name":-1});
 
 //devolver _id = 22
-db.estudiantes.find({_id:"17"});
+//db.estudiantes.find({"_id":"17"});
+
+// devuelve sólo el nombre y la dirección del estudiante con id igual 22
+//db.estudiantes.find({"_id":"17"},{"name":1, "address":1, "_id":0});
+
+//estudiantes que son de  BROOKLYN
+//db.estudiantes.find({"address.city":"BROOKLYN"});
+
+
+//un estudiante de BROOKLYN
+//db.estudiantes.findOne({"address.city":"BROOKLYN"});
+
+//estudiantes que son de BROOKLYN o MANHTTAN
+//db.estudiantes.find({$or:[{"address.city":"BROOKLYN"},{"address.city":"MANHTTAN"}]});
+
+//estudiantes cuya nota media es mayor o igual que 4
+//db.estudiantes.find({"notaMedia":{$gt:4}});
+
+//estudiantes cuya nota media es mayor o igual que 4 y menor que 6
+//db.estudiantes.find({"notaMedia":{$gte:4, $lt:6}});
+
+//estudiantes tienen de nota media 9 o más.
+db.estudiantes.find({"notaMedia":{$gte:9}});
