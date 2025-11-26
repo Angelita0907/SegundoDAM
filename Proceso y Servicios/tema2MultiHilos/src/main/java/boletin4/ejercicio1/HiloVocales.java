a8f67138b6a31db9d28225dc901d355f7a4e604b
Angela <achimon0907@g.educaand.es>
2025-11-26 11:35:42 +0100

    terminar boletin 4

--- Proceso y Servicios/tema2MultiHilos/src/main/java/boletin4/ejercicio1/HiloVocales.java
+++ Proceso y Servicios/tema2MultiHilos/src/main/java/boletin4/ejercicio1/HiloVocales.java
@@ -1,7 +1,47 @@
 package boletin4.ejercicio1;
 
-public class HiloVocales {
+public class HiloVocales extends Thread {
+
+	private char vocal;
+	private int numVocales;
+	private CuentaVocales vocales;
+
+	public HiloVocales(char vocal, int numVocales, CuentaVocales vocales) {
+		super();
+		this.vocal = vocal;
+		this.numVocales = numVocales;
+		this.vocales = vocales;
+	}
+
+	public char getVocal() {
+		return vocal;
+	}
+
+	public void setVocal(char vocal) {
+		this.vocal = vocal;
+	}
+
+	public int getNumVocales() {
+		return numVocales;
+	}
+
+	public void setNumVocales(int numVocales) {
+		this.numVocales = numVocales;
+	}
+
+	public CuentaVocales getVocales() {
+		return vocales;
+	}
+
+	public void setVocales(CuentaVocales vocales) {
+		this.vocales = vocales;
+	}
 	
 	
+	public void cuentaVocales(char vocal, String texto) {
+		
+		
+		
+	}
 
 }
