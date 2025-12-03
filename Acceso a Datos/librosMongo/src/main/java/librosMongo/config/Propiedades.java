b7417407c11857a6ba7212a60b1e979c024ee785
Angela <achimon0907@g.educaand.es>
2025-12-03 21:53:08 +0100

    seguir proyecto mongo

---
+++ Acceso a Datos/librosMongo/src/main/java/librosMongo/config/Propiedades.java
@@ -0,0 +1,26 @@
+package librosMongo.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import librosMongo.config.Propiedades;
+
+public class Propiedades {
+private final static String rutaResources = "src\\main\\resources\\";
+	
+	private static final Logger logger = LogManager.getLogger(Propiedades.class);
+	
+	private Properties props = new Properties();
+
+	public Propiedades(String nombreFichero) throws IOException {
+		props.load(new FileInputStream(rutaResources + nombreFichero));
+	}
+
+	public String get(String key) {
+		return props.getProperty(key);
+	}
+}
