9fd32b3a89e346c9709ef7d9d9786a635b54c54d
Angela <achimon0907@g.educaand.es>
2025-12-11 20:37:33 +0100

    seguir proyecto

---
+++ Acceso a Datos/jdcbDB/src/main/java/jdbc/controller/DixitControlador.java
@@ -0,0 +1,10 @@
+package jdbc.controller;
+
+public class DixitControlador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Acceso a Datos/jdcbDB/src/main/java/jdbc/utiles/MiExcepcion.java
@@ -0,0 +1,14 @@
+package jdbc.utiles;
+
+public class MiExcepcion extends Exception{
+
+	public MiExcepcion(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+
+}
